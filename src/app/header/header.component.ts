fb65accbff04e3d3db8bfa77b8bf1fdeada53c30
chesteric31 <eric.binard.182@gmail.com>
2023-11-29 22:51:50 +0100

    custom material theme

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {FormsModule} from "@angular/forms";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
